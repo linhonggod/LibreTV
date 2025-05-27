d65c83aa49a93aacd8dcdd7dca48468cbac059b3
linhonggod <linhonggod@gmail.com>
2025-05-27 22:24:23 +0800

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -39,7 +39,7 @@ app.use(cors({
 
 app.use((req, res, next) => {
   res.setHeader('X-Content-Type-Options', 'nosniff');
-  res.setHeader('X-Frame-Options', 'SAMEORIGIN');
+  res.setHeader('X-Frame-Options', 'DENY');
   res.setHeader('X-XSS-Protection', '1; mode=block');
   next();
 });
